e957889c8287550139421427dce4fa9e763cf1c3
ahlemkaabi <ahlemkaabi1412@gmail.com>
2021-06-01 23:09:45 +0100

    docu

--- 0x0A-python-inheritance/9-rectangle.py
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -9,10 +9,10 @@ class Rectangle(BaseGeometry):
     """ define class rectangle """
 
     def __init__(self, width, height):
+        super().integer_validator("width", width)
+        super().integer_validator("height", height)
         self.__width = width
         self.__height = height
-        self.integer_validator("width", width)
-        self.integer_validator("height", height)
 
     def __str__(self):
         return "[Rectangle] {}/{}".format(self.__width, self.__height)

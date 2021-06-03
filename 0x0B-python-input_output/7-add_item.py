dc1b25374982c14c527e2c76826553bb2ee3a70c
ahlemkaabi <ahlemkaabi1412@gmail.com>
2021-06-03 15:32:19 +0100

    add args list to a file json

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -2,9 +2,12 @@
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 import json
-
-def get_args(*args):
-    args_list = []
-    for arg in args:
-        save_to_json_file(args_list.append(arg), 'add_item.json')
-    load_from_json_file('add_item.json')
+import sys
+args = sys.argv[1:]
+with open('add_item.json', 'a') as file:
+    try:
+        l = load_from_json_file('add_item.json')
+    except:
+        l = []
+    l += args
+    save_to_json_file(l, 'add_item.json')

c9ebc6a64d14a50ae6befdbbf42e5ce71972d469
ahlemkaabi <ahlemkaabi1412@gmail.com>
2021-06-03 16:43:27 +0100

    class instnce dict representation

--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -8,13 +8,13 @@ class Student:
     '''
 
     def __init__(self, first_name, last_name, age):
-        '''class
+        '''class initialization
         '''
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
     def to_json(self):
-        '''class2
+        '''class2: to json representation
         '''
         return self.__dict__

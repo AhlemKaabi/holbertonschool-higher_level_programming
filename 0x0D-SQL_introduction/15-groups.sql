677a70084385ae3880eb82ba064f92ccbbc7e2ab
ahlemkaabi <ahlemkaabi1412@gmail.com>
2021-07-13 17:46:07 +0100

    groups

---
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -0,0 +1,10 @@
+-- script that lists the number of records with the same score
+-- in the table second_table of the database hbtn_0c_0 in your MySQL server.
+--    The result should display:
+--        the score
+--        the number of records for this score with the label number
+
+SELECT score, COUNT(score) AS number
+FROM second_table
+GROUP BY score
+ORDER BY score desc;

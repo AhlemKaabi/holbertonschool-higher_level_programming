75152d7781b6ef36e4370d1c22460b9c1272f08d
ahlemkaabi <ahlemkaabi1412@gmail.com>
2021-07-13 16:00:50 +0100

    select oerdered by

---
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -0,0 +1,9 @@
+-- script that lists all records of the table second_table of the database hbtn_0c_0 in your MySQL server
+--  score   name
+--  14  Bob
+--  10  John
+--  8   George
+--  3   Alex
+SELECT name, score
+FROM second_table
+ORDER BY score ASC | DESC;

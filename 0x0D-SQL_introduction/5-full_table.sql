7e118825ad86d9bbf48f9773655dea5a90cf58d7
ahlemkaabi <ahlemkaabi1412@gmail.com>
2021-07-13 14:36:25 +0100

    full table

--- 0x0D-SQL_introduction/5-full_table.sql
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -1,3 +1,2 @@
 -- script that prints the full description of the table first_table
-SELECT * FROM INFORMATION_SCHEMA.COLUMNS where TABLE_NAME='first_table'
-
+SELECT * FROM INFORMATION_SCHEMA.COLUMNS where TABLE_NAME='first_table';

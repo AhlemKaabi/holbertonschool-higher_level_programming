f7b4183f4020cb1bcdc53bb94f7c96ebf9a62d3a
ahlemkaabi <ahlemkaabi1412@gmail.com>
2021-07-13 01:49:33 +0100

    test for comment syntax

--- 0x0D-SQL_introduction/3-list_tables.sql
+++ 0x0D-SQL_introduction/3-list_tables.sql
@@ -1,2 +1,2 @@
-/*a script that lists all the tables of a database(mysql) in your MySQL server.*/
+-- a script that lists all the tables of a database(mysql) in your MySQL server.*/
 SHOW TABLES FROM mysql;

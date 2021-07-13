a7e83a7a57ccffbbcd174909e3e2c6db8942edfe
ahlemkaabi <ahlemkaabi1412@gmail.com>
2021-07-13 02:04:48 +0100

    database name as argument fix comment syntax

--- 0x0D-SQL_introduction/3-list_tables.sql
+++ 0x0D-SQL_introduction/3-list_tables.sql
@@ -1,3 +1,3 @@
-/* A script that lists all the tables of a database(mysql) in your MySQL server.
-   The database name will be passed as argument of mysql*/
+-- A script that lists all the tables of a database(mysql) in your MySQL server.
+-- The database name will be passed as argument of mysql
 SHOW TABLES;

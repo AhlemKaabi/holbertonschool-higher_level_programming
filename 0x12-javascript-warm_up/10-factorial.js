b838dc45f4596ebcabcd49c603ff58883e464863
ahlemkaabi <ahlemkaabi1412@gmail.com>
2021-08-02 23:09:31 +0100

    fix js style + add condition for NaN

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,10 +1,10 @@
 #!/usr/bin/node
-let arg1 = parseInt(process.argv[2]);
-function factorial(number) {
+const arg1 = parseInt(process.argv[2]);
+function factorial (number) {
   if (number < 0) {
     return (-1);
-  } else if (number == 0) {
-    return 1;
+  } else if (number === 0 || isNaN(number)) {
+    return (1);
   } else {
     return (number * factorial(number - 1));
   }

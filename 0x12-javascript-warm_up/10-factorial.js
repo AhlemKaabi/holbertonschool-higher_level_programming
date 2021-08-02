a2a358906cb956d3b5ffb748421ce19303ed5db2
ahlemkaabi <ahlemkaabi1412@gmail.com>
2021-08-02 22:56:42 +0100

    test

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,11 +1,12 @@
 #!/usr/bin/node
 let arg1 = parseInt(process.argv[2]);
-let result = arg1;
-if (arg1 === 0 || arg1 === 1 || arg1 === null || process.argv[2] === null) {
-  console.log(1);
+function factorial(number) {
+  if (number < 0) {
+    return (-1);
+  } else if (number == 0) {
+    return 1;
+  } else {
+    return (number * factorial(number - 1));
+  }
 }
-while (arg1 > 1) {
-  arg1--;
-  result = result * arg1;
-}
-console.log(result);
+console.log(factorial(arg1));

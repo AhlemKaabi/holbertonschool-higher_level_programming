1b1421503962260d853813007efd3f2cc16edad9
ahlemkaabi <ahlemkaabi1412@gmail.com>
2021-08-02 18:11:48 +0100

    for loop

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,5 @@
+#!/usr/bin/node
+const arrayname = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+for (let index = 0; index < arrayname.length; index++) {
+  console.log(arrayname[index]);
+}

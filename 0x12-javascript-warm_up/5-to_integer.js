286383ffd5dad4b29f9dad13a361972747c71a9e
ahlemkaabi <ahlemkaabi1412@gmail.com>
2021-08-02 17:45:53 +0100

    fix js style

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,10 +1,10 @@
 #!/usr/bin/node
 const args = process.argv;
-let MyNumber = parseInt(args[2], 10);
+const MyNumber = parseInt(args[2], 10);
 if (args[2] == null) {
-  console.log('Not a number')
+  console.log('Not a number');
 } else if (isNaN(MyNumber)) {
-  console.log('Not a number')
+  console.log('Not a number');
 } else {
   console.log('My number: ' + MyNumber);
 }

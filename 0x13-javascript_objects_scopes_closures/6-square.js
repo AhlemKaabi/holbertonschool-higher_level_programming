1320fb1d4e37239670b7f612b73d3e9150711401
ahlemkaabi <ahlemkaabi1412@gmail.com>
2021-08-04 23:24:48 +0100

    fix code add constructor function fix variable

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -2,13 +2,17 @@
 const SquareClass = require('./5-square.js');
 
 class Square extends SquareClass {
+  constructor (size) {
+      super(size);
+  }
+
   charPrint (c) {
     for (let j = 0; j < this.width; j++) {
       for (let j = 0; j < this.width; j++) {
         if (c === undefined) {
           process.stdout.write('X');
         } else {
-          process.stdout.write('c');
+          process.stdout.write(c);
         }
       }
       console.log('');

76f7f2d8d76bacb68b0b9aa699f0d69f5638190d
ahlemkaabi <ahlemkaabi1412@gmail.com>
2021-08-04 21:58:24 +0100

    fix the required file name

--- 0x13-javascript_objects_scopes_closures/5-square.js
+++ 0x13-javascript_objects_scopes_closures/5-square.js
@@ -1,11 +1,10 @@
 #!/usr/bin/node
 
-const Rectangle = require('./2-rectangle');
+const Rectangle = require('./4-rectangle.js');
 
 class Square extends Rectangle {
   constructor (size) {
     super(size, size);
-    this.size = size;
   }
 }
 module.exports = Square;

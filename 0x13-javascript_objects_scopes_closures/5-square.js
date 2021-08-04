47e752c8138fe1aad1b4ecb92dabe1bb4b63b965
ahlemkaabi <ahlemkaabi1412@gmail.com>
2021-08-04 21:55:01 +0100

    fix class call

--- 0x13-javascript_objects_scopes_closures/5-square.js
+++ 0x13-javascript_objects_scopes_closures/5-square.js
@@ -4,25 +4,8 @@ const Rectangle = require('./2-rectangle');
 
 class Square extends Rectangle {
   constructor (size) {
-    super(size);
+    super(size, size);
     this.size = size;
-  }
-
-  print () {
-    for (let j = 0; j < this.size; j++) {
-      for (let j = 0; j < this.size; j++) {
-        process.stdout.write('X');
-      }
-      console.log('');
-    }
-  }
-
-  double () {
-    this.size = this.size * 2;
-  }
-
-  rotate () {
-    this.size = this.size;
   }
 }
 module.exports = Square;

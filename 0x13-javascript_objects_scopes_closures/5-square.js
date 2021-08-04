95d739f9bab80de8a289350dbb37ab3787109f2b
ahlemkaabi <ahlemkaabi1412@gmail.com>
2021-08-04 21:41:49 +0100

    inherited class

---
+++ 0x13-javascript_objects_scopes_closures/5-square.js
@@ -0,0 +1,27 @@
+#!/usr/bin/node
+
+const Rectangle = require('./2-rectangle');
+
+class Square extends Rectangle {
+  constructor (size) {
+    super(size);
+    this.size = size;
+  }
+
+  print () {
+    for (let j = 0; j < this.size; j++) {
+      for (let j = 0; j < this.size; j++) {
+        process.stdout.write('X');
+      }
+      console.log('');
+    }
+  }
+
+  double () {
+    this.size = this.size * 2;
+  }
+
+  rotate () {
+  }
+}
+module.exports = Square;

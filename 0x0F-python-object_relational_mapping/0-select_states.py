ea014886363d5babed68a16c1ea1bf792704499d
ahlemkaabi <ahlemkaabi1412@gmail.com>
2021-08-07 14:18:18 +0100

    fix pep8 style

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,30 +1,27 @@
 #!/usr/bin/python3
-# script that lists all states from the database hbtn_0e_0_usa
+''' script that lists all states from the database hbtn_0e_0_usa
+'''
+
 if __name__ == "__main__":
-	import MySQLdb
-	from sys import argv
-	''' importing the MySQLdb is an interface to the popular MySQL database server that
-		provides the Python database API.
-	'''
-	# Open database connection
-	db = MySQLdb.connect(
-		host = "localhost",
-		user = argv[1],
-		password = argv[2],
-		database = argv[3],
-	)
-	# prepare a cursor object using cursor() method
-	cursor = db.cursor()
-	# sql query string to be executed on the database
-	sql = "SELECT * FROM states ORDER BY id"
-	# Execute the SQL command
-	cursor.execute(sql)
-	# Fetch all the rows in a list of lists.
-	results = cursor.fetchall()
-	# Now print fetched result
-	for row in results:
-		print(row)
-	cursor.close()
-	db.close()
-
-
+    import MySQLdb
+    from sys import argv
+    # Open database connection
+    db = MySQLdb.connect(
+        host="localhost",
+        user=argv[1],
+        password=argv[2],
+        database=argv[3],
+    )
+    # prepare a cursor object using cursor() method
+    cursor = db.cursor()
+    # sql query string to be executed on the database
+    sql = "SELECT * FROM states ORDER BY id"
+    # Execute the SQL command
+    cursor.execute(sql)
+    # Fetch all the rows in a list of lists.
+    results = cursor.fetchall()
+    # Now print fetched result
+    for row in results:
+        print(row)
+    cursor.close()
+    db.close()

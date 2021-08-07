d45c8f7d6c7ad6f06a642651065ff6f96d7a417c
ahlemkaabi <ahlemkaabi1412@gmail.com>
2021-08-08 00:02:35 +0100

    sqlalchemy

---
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Start link class to table in database
+"""
+import sys
+from sqlalchemy import create_engine
+from sqlalchemy import Column
+from sqlalchemy import String
+from sqlalchemy import Integer
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+class state(Base):
+    __tablename__ = "states"
+    id = Column(Integer, primary_key=True, autoincrement=True, )
+    name = Column(String(256),)

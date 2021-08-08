6b6c2d1d36437115744753d93579a7147e97a0d0
ahlemkaabi <ahlemkaabi1412@gmail.com>
2021-08-08 15:58:27 +0100

    works!

---
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+"""script that adds the State object “Louisiana” to
+    the database hbtn_0e_6_usa
+"""
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+import sys
+
+if __name__ == '__main__':
+    import model_state
+    from model_state import Base, State
+    # connect to the DB
+    engine = create_engine(
+        'mysql+mysqldb://{}:{}@localhost/{}'.
+        format(sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
+    # talk to the DB: usning sessions
+
+    # create a configured "Session" class
+    Session = sessionmaker(bind=engine)
+
+    Base.metadata.create_all(engine)
+
+    # create a Session
+    mySession = Session()
+    mySession.add(State(name='Louisiana'))
+    mySession.commit()
+    # print(Session1.query(model_state.state).order_by(model_state.state.id).all())
+    mySessionQuery = mySession.query(model_state.State).\
+        filter(model_state.State.name == 'Louisiana')
+    if mySessionQuery:
+        for instance in mySessionQuery:
+            print(instance.id)
+    else:
+        print("Not found")
+    mySession.close()

050fda8c0da4cfc1d144a824d4f595e1abb3bbac
ahlemkaabi <ahlemkaabi1412@gmail.com>
2021-08-08 13:44:57 +0100

    last test

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -7,22 +7,22 @@ from sqlalchemy.orm import sessionmaker
 import model_state
 from model_state import Base
 import sys
+if __name__ == '__main__':
+    # connect to the DB
+    engine = create_engine(
+        'mysql+mysqldb://{}:{}@localhost/{}'.
+        format(sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
+    # Access the tables (like cursor)
+    Base.metadata.create_all(engine)
+    # talk to the DB: usning sessions
 
-# connect to the DB
-engine = create_engine(
-    'mysql+mysqldb://{}:{}@localhost/{}'.
-    format(sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
-# Access the tables (like cursor)
-Base.metadata.create_all(engine)
-# talk to the DB: usning sessions
+    # create a configured "Session" class
+    Session = sessionmaker(bind=engine)
+    # create a Session
+    mySession = Session()
+    # print(Session1.query(model_state.state).order_by(model_state.state.id).all())
+    for st_obj in mySession.query(model_state.State).\
+                order_by(model_state.State.id).all():
+        print("{}: {}".format(st_obj.id, st_obj.name))
 
-# create a configured "Session" class
-Session = sessionmaker(bind=engine)
-# create a Session
-mySession = Session()
-# print(Session1.query(model_state.state).order_by(model_state.state.id).all())
-for st_obj in mySession.query(model_state.State).\
-            order_by(model_state.State.id).all():
-    print("{}: {}".format(st_obj.id, st_obj.name))
-
-mySession.close()
+    mySession.close()

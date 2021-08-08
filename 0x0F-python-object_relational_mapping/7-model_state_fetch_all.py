42cfd5b78e399f877df67bcbced12a53628555f5
ahlemkaabi <ahlemkaabi1412@gmail.com>
2021-08-08 12:11:11 +0100

    fix class name

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-from typing import Sized
 from sqlalchemy import create_engine
 from sqlalchemy.orm import Session
+from sqlalchemy.orm.session import Session
 import model_state
 from model_state import Base
 import sys
@@ -13,11 +13,11 @@ engine = create_engine(
 # Access the tables (like cursor)
 Base.metadata.create_all(engine)
 # talk to the DB: usning sessions
-Session1 = Session(engine)
+Session1 = Session(bind=engine)
 # print(Session1.query(model_state.state).order_by(model_state.state.id).all())
-for state in Session1.query(model_state.state).\
-            order_by(model_state.state.id).all():
+for st_obj in Session1.query(model_state.State).\
+            order_by(model_state.State.id).all():
 
-    print("{}: {}".format(state.id, state.name))
+    print("{}: {}".format(st_obj.id, st_obj.name))
 
 Session1.close()

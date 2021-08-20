6cce7c765232852c8e5725ac1384400691732265
ahlemkaabi <ahlemkaabi1412@gmail.com>
2021-08-20 16:43:11 +0100

    executable files

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -5,6 +5,7 @@
     found in the header of the response.
 """
 import sys, urllib.request
+
 if __name__ == "__main__":
     with urllib.request.urlopen(sys.argv[1]) as response:
         print(response.getheader("X-Request-Id"))

2724f24a4608492a06ba70bcf4c656dd3af52f36
ahlemkaabi <ahlemkaabi1412@gmail.com>
2021-09-14 13:02:31 +0100

    first move

---
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+
+// take the file path as argument
+if (process.argv.length == 3) {
+	const fs = require('fs');
+
+	fs.readFile(process.argv[2], 'utf8' , (err, data) => {
+	if (err) {
+		console.error(err);
+		return
+	}
+	console.log(data);
+	});
+}

f9fb122dc49238eea03ffb3e4aa09f212266b616
ahlemkaabi <ahlemkaabi1412@gmail.com>
2021-09-14 18:35:08 +0100

    fix semistandard

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -1,20 +1,21 @@
 #!/usr/bin/node
 const request = require('request');
-let data = {}
+const data = {};
+let task = 0;
 request(process.argv[2], function (error, response, body) {
   if (error) throw error;
   const content = JSON.parse(body);
-  for (task in content){
-	if (!(content[task].userId in data)){
-		data[content[task].userId] = 0;
-		if ( content[task].completed === true ) {
-			data[content[task].userId] += 1;
-		}
-	} else {
-		if ( content[task].completed === true ) {
-			data[content[task].userId] += 1;
-		}
-	}
+  for (task in content) {
+    if (!(content[task].userId in data)) {
+      data[content[task].userId] = 0;
+      if (content[task].completed === true) {
+        data[content[task].userId] += 1;
+      }
+    } else {
+      if (content[task].completed === true) {
+        data[content[task].userId] += 1;
+      }
+    }
   }
   console.log(data);
 });
